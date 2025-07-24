0cdb70aa31ccf9eeb63107642a79fdd2901fab62
jja42 <jja42w@gmail.com>
2025-07-24 10:34:50 -0400

    serving image at assets/logo.png

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets", http.FileServer(http.Dir("./assets")))
 	server := http.Server{Addr: ":8080", Handler: mux}
 	server.ListenAndServe()
 }
