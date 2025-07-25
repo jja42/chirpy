dfd72f8db386bd554e211089a4f00a5b5c2e059a
jja42 <jja42w@gmail.com>
2025-07-25 11:19:54 -0400

    Metrics

--- handlers.go
+++ handlers.go
@@ -1,9 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func handlerReadiness(writer http.ResponseWriter, req *http.Request) {
 	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	writer.WriteHeader(200)
 	writer.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(writer, req)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(writer http.ResponseWriter, req *http.Request) {
+	str := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	writer.Write([]byte(str))
+}
+
+func (cfg *apiConfig) handlerReset(writer http.ResponseWriter, req *http.Request) {
+	cfg.fileserverHits.Store(0)
 }
--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 
+	var apiCfg apiConfig
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(http.FileServer(http.Dir(".")))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 	server := http.Server{Addr: ":8080", Handler: mux}
 	server.ListenAndServe()
 }
